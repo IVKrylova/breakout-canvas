e719b56958e8da8b09d3d29f32d39265cdbcd00b
IVKrylova <ivk.lis@yandex.ru>
2022-08-20 14:28:56 +0300

    feat: bounce off the walls

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,24 @@
 import './index.css';
+const {
+  FACTOR_X,
+  FACTOR_Y,
+  BALL_RADIUS,
+} = require('../utils/constants');
 
 const canvas = document.querySelector('.canvas');
 // переменная для хранения 2D визуализации контекста
 const ctx = canvas.getContext('2d');
 // начальная позиция мяча
-let x = canvas.width/2;
-let y = canvas.height-30;
+let x = canvas.width/FACTOR_X;
+let y = canvas.height-FACTOR_Y;
 // изменение координат при движении мяча
-const dx = 2;
-const dy = -2;
+let dx = 2;
+let dy = -2;
 
 // функция отрисовки мяча
 const drawBall = _ => {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.arc(x, y, BALL_RADIUS, 0, Math.PI*2);
   ctx.fillStyle = "#0095dd";
   ctx.fill();
   ctx.closePath();
@@ -27,6 +32,10 @@ const draw = _ => {
   drawBall();
   x += dx;
   y += dy;
+
+  // отскоки мяча от стенки
+  if (x + dx > canvas.width - BALL_RADIUS || x + dx < BALL_RADIUS) dx = -dx;
+  if (y + dy > canvas.height - BALL_RADIUS || y + dy < BALL_RADIUS) dy = -dy;
 }
 
 // заставляем мяч двигаться
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+// коэффициенты для вычисления начальной позиции мяча
+export const FACTOR_X = 2;
+export const FACTOR_Y = 30;
+
+// радиус мяча
+export const BALL_RADIUS = 10;
